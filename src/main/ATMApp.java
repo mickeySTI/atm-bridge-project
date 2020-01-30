85f3098d65fba2c45d4ad94f34bbad55b6f740c6
mickeySTI <imickey216@gmail.com>
2020-01-30 00:18:17 -0500

    final

--- src/main/ATMApp.java
+++ src/main/ATMApp.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class ATMApp {
@@ -11,11 +12,21 @@ public class ATMApp {
 	
 		String setPin = Atm.getPin();
 		
+	
+		
+		 BigDecimal newBalance = new BigDecimal(Atm.getBalance());
 
+		
+		 
+		 
+		 
 		int attempts = 1;
 
 		System.out.print("Please enter your pin: ");
 		String enteredPin = input.next();
+		
+		
+		
 		while (!enteredPin.equals(setPin)){
 			System.out.println("Sorry that is not the correct PIN, please enter your pin: ");
 			enteredPin = input.next();
@@ -27,11 +38,56 @@ public class ATMApp {
 			
 		}
 
-		if (enteredPin.equals(setPin)) {
-			System.out.println("Congrats you entered the right pin!");
-
+		int userOption = 0;
+		System.out.println("Congrats you entered the correct pin!\n");
+		System.out.println("Welcome to We Can Code It Bank!");
+		while(enteredPin.equals(setPin) && userOption != 4) {
+			displayOptions();
+			userOption = input.nextInt();
+			if(userOption == 1) {
+				System.out.println("How much would you like to deposit?");
+				double depositAmount = input.nextDouble();
+				Atm.deposit(depositAmount);
+			}else if(userOption == 2) {
+				System.out.println("How much would you like to withdraw");
+				double withdrawAmount = input.nextDouble();
+				Atm.withdraw(withdrawAmount);
+			}else if(userOption == 3) {
+				System.out.println("Your current balance is " + Atm.getBalance());
+			}
+			else if(userOption == 4) {
+				System.out.println("Thank you for choosing We Can Code It Bank!");
+				break;
+			}
+			else {
+				System.out.println("Please choose from 1-4");
+			}
+		
+		
 		}
+		
+		
 		input.close();
 	}
+	
+	
+	
+	
+	
+	
+	public static void displayOptions() {
+		System.out.println("Choose an option.\r\n" + 
+				"Press 1 to deposit funds.\r\n" + 
+				"Press 2 to withdraw funds.\r\n" + 
+				"Press 3 to check balance.\r\n" + 
+				"Press 4 to exit.");
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
